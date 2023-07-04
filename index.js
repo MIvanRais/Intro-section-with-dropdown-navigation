5780e1720ec2a0ca6dd633d67d8158677db7dcac
MIvanRais <26ivanrais@gmail.com>
2023-07-04 15:03:54 +0700

    Create The Interactive  Navigation Side And Style It For Small Devices

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// interactive header
 const links = document.querySelectorAll('.with-icon');
 const svgs = document.querySelectorAll('.with-icon>svg>path');
 const topLevelEntryContainers = document.querySelectorAll('.top-level-entry-container');
@@ -38,6 +39,63 @@ links[1].addEventListener('click', (e) => {
 
         setTimeout(() => {
             links[1].classList.remove('inactiveSvg');
+        }, 500);
+    }
+})
+
+// interactive nav side
+const burgerMenu = document.querySelector('#burger-menu');
+const navSide = document.querySelector('main>nav');
+const btnClose = document.querySelector('#btn-close');
+const body = document.querySelector('body');
+const subMenusSide = document.querySelectorAll('.sub-menu');
+
+burgerMenu.addEventListener('click', () => {
+    navSide.classList.add('nav-side');
+    body.style.overflowY = 'hidden';
+})
+
+btnClose.addEventListener('click', () => {
+    navSide.classList.remove('nav-side');
+    body.style.overflowY = 'visible';
+})
+
+links[2].addEventListener('click', () => {
+    subMenusSide[0].classList.toggle('show-sub-menu-side');
+    if (!links[2].classList.contains('activeSvg')) {
+        links[2].classList.add('activeSvg');
+        links[2].classList.add('activeLink');
+        svgs[2].classList.add('activePath');
+    }
+    else if (links[2].classList.contains('activeSvg')) {
+        links[2].classList.remove('activeSvg');
+        links[2].classList.remove('activeLink');
+        svgs[2].classList.remove('activePath');
+
+        links[2].classList.add('inactiveSvg');
+
+        setTimeout(() => {
+            links[2].classList.remove('inactiveSvg');
+        }, 500);
+    }
+})
+
+links[3].addEventListener('click', () => {
+    subMenusSide[1].classList.toggle('show-sub-menu-side');
+    if (!links[3].classList.contains('activeSvg')) {
+        links[3].classList.add('activeSvg');
+        links[3].classList.add('activeLink');
+        svgs[3].classList.add('activePath');
+    }
+    else if (links[3].classList.contains('activeSvg')) {
+        links[3].classList.remove('activeSvg');
+        links[3].classList.remove('activeLink');
+        svgs[3].classList.remove('activePath');
+
+        links[3].classList.add('inactiveSvg');
+
+        setTimeout(() => {
+            links[3].classList.remove('inactiveSvg');
         }, 500);
     }
 })
